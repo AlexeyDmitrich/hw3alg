aa2837aa70a1e30780915b1d4af344d17102a34f
alex <a.d.kamenev@yandex.ru>
2022-09-22 22:57:44 +0300

    v 0.0.2 в разработке метод генерации. отрабатывает 0 вместо пропуска.

--- src/main/java/MainHW3.java
+++ src/main/java/MainHW3.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * description:
  * 1. Дан массив из n элементов, начиная с 1. Каждый следующий элемент равен (предыдущий + 1). Но в массиве гарантированно 1 число пропущено. Необходимо вывести на экран пропущенное число. Постарайтесь придумать более оптимальный (асимптотически) алгоритм. Примеры:
@@ -13,7 +15,35 @@
 
 public class MainHW3 {
     public static void main(String[] args) {
-
+        int[] test = generator(15);
+        for (int num:
+             test) {
+            System.out.print(num + ", ");
+        }
     }
 
+    /**
+     * метод для генерации случайного массива от 1 до n с одни пропущеным значением
+     * @param length
+     * @return
+     */
+    public static int[] generator(int length){
+        Random rnd = new Random();
+        int xNumber = rnd.nextInt(length);
+        int start = 1;
+        int[] arr = new int[length];
+        for (int i = 0; i < xNumber; i++) {
+            arr[i] = start;
+            start ++;
+        }
+        start++;
+        for (int i = xNumber+1; i < arr.length; i++) {
+            arr[i] = start;
+            start++;
+        }
+
+
+
+        return arr;
+    }
 }
