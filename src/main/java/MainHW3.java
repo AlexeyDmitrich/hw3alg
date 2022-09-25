3d0108e5172c1b41f356f8f7ada02c03da63f506
alex <a.d.kamenev@yandex.ru>
2022-09-25 23:27:03 +0300

    v 1.2.1
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	.idea/workspace.xml
    #	src/main/java/MainHW3.java

--- src/main/java/MainHW3.java
+++ src/main/java/MainHW3.java
@@ -3,60 +3,19 @@ import java.util.Random;
 import java.util.function.Consumer;
 
 /**
- * description:
- * 1. Дан массив из n элементов, начиная с 1. Каждый следующий элемент равен (предыдущий + 1). Но в массиве гарантированно 1 число пропущено. Необходимо вывести на экран пропущенное число. Постарайтесь придумать более оптимальный (асимптотически) алгоритм. Примеры:
- *
- * 1. [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16] => 11
- * 2. [1, 2, 4, 5, 6] => 3
- * 3. [2, 3, 4, 5, 6, 7, 8] => 1
- * 4. [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] => 15
- * 5. [] => 1
- *
- * 2. *Попробуйте реализовать двунаправленную очередь (чтобы элементы можно было добавлять и удалять не только в начало (из начала), но и в конец (с конца))
+ * Комментарий к домашнему заданию:
+ * много времени потратил на класс для тестирования методов, в расчёте, что найду
+ * хотя бы 3 штуки. В итоге, так увлёкся, что адекватным получился только способ тестирования.
+ * На большее не хватает времени. Надеюсь, скоро смогу собрать машину, которая не будет думать
+ * полчаса над каждым вводимым символом)
  */
 
 public class MainHW3 {
-    static Random rnd = new Random();
 
     public static void main(String[] args) {
         TestClass.uniMethod(MainHW3::searchLose);
 
     }
-
-    // /**
-    // * Метод для быстрой обкатки работоспособности
-    // */
-    //static void quickTest (){
-    //     int[] test  = generator(rnd.nextInt(48));
-    //     int[] test1 = {1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16}; // => 11
-    //     int[] test2 = {1, 2, 4, 5, 6};  // => 3
-    //     int[] test3 = {2, 3, 4, 5, 6, 7, 8}; // => 1
-    //     int[] test4 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}; // => 15
-    //     int[] test5 = { }; // => 1
-//
-    //    for (int num: test) {
-    //        System.out.print(num + " ");
-    //    }
-    //    System.out.println();
-    //    int res = searchLose(test5);
-    //    System.out.println("В массиве пропущено число " + res);
-    //    System.out.println();
-//
-    //    int res1 = searchLose(test1);
-    //    int res2 = searchLose(test2);
-    //    int res3 = searchLose(test3);
-    //    int res4 = searchLose(test4);
-    //    int res5 = searchLose(test5);
-    //    if (res1 == 11 && res2 == 3 && res3 == 1 && res4 == 15 && res5 == 1) {
-    //        System.out.println("Все автотесты пройдены успешно");
-    //    } else {
-    //        System.out.printf("Тест 1: %d ожидание: 11, \n" +
-    //                "Тест 2: %d ожидание: 3, \n" +
-    //                "Тест 3: %d ожидание: 1, \n" +
-    //                "Тест 4: %d ожидание: 15, \n" +
-    //                "Тест 5: %d ожидание: 1, \n", res1, res2, res3, res4, res5);
-    //    }
-    //}
 
     /**
      * Самое очевидное решение поиска - идти подряд по всем элементам
@@ -78,29 +37,5 @@ public class MainHW3 {
         searchedNum = array.length + 1;    // если пропавшего числа не нашлось, значит оно было последним
         return searchedNum;
     }
-
-    ///**
-    // * метод для генерации случайного массива от 1 до n с одни пропущеным значением
-    // * @param length
-    // * @return
-    // */
-    //public static int[] generator(int length){
-    //    int xNumber = (rnd.nextInt(length-2)+2);
-    //    int start = 1;
-    //    int[] arr = new int[length];
-    //    for (int i = 0; i < xNumber; i++) {
-    //        arr[i] = start;
-    //        start ++;
-    //    }
-    //    start++;
-    //    for (int i = xNumber; i < arr.length; i++) {
-    //        arr[i] = start;
-    //        start++;
-    //    }
-    //    return arr;
-    //}
-
-
-
 
 }
